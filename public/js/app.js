758c55911bcaa658dfd5398c08baad40efdcd437
Treasure Kabareebe <treasurecalm@gmail.com>
2020-11-04 02:49:33 +0300

    Modify the TimerForm buttons to specify onClick events

--- public/js/app.js
+++ public/js/app.js
@@ -184,10 +184,16 @@ class TimerForm extends React.Component {
               />
             </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
+              <button
+                className='ui basic blue button'
+                onClick={this.handleSubmit}
+              >
                 {submitText}
               </button>
-              <button className='ui basic red button'>
+              <button
+                className='ui basic red button'
+                onClick={this.props.onFormClose}
+              >
                 Cancel
               </button>
             </div>
