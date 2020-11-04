5f44ffa100078e9085eeef1a1ae6ff4274d2ee0d
Treasure Kabareebe <treasurecalm@gmail.com>
2020-11-04 03:01:23 +0300

    Pass down onFormClose() and onFormSubmit() functions to TimerForm

--- public/js/app.js
+++ public/js/app.js
@@ -45,11 +45,21 @@ class ToggleableTimerForm extends React.Component {
     this.setState({ isOpen: true });
   };
 
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleFormSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.setState({ isOpen: false });
+  };
+
   render() {
     if (this.state.isOpen) {
-      return (
-        <TimerForm />
-      );
+      <TimerForm
+        onFormSubmit={this.handleFormSubmit}
+        onFormClose={this.handleFormClose}
+      />
     } else {
       return (
         <div className='ui basic content center aligned segment'>
