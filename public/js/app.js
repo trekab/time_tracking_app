c3dc956dbbcffc68f2c99ad30076a69d243a79dc
Treasure Kabareebe <treasurecalm@gmail.com>
2020-11-06 03:04:17 +0300

    Run events through EditableTimer and EditableTimerList

--- public/js/app.js
+++ public/js/app.js
@@ -133,6 +133,8 @@ class EditableTimerList extends React.Component {
         runningSince={timer.runningSince}
         onFormSubmit={this.props.onFormSubmit}
         onTrashClick={this.props.onTrashClick}
+        onStartClick={this.props.onStartClick}
+        onStopClick={this.props.onStopClick}
       />
     ));
     return (
@@ -190,6 +192,8 @@ class EditableTimer extends React.Component {
           runningSince={this.props.runningSince}
           onEditClick={this.handleEditClick}
           onTrashClick={this.props.onTrashClick}
+          onStartClick={this.props.onStartClick}
+          onStopClick={this.props.onStopClick}
         />
       );
     }
