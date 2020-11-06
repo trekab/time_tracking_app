f1a821cfc78c044130c748b368f1fee499325fcf
Treasure Kabareebe <treasurecalm@gmail.com>
2020-11-06 03:01:07 +0300

    Create TimerActionButton

--- public/js/app.js
+++ public/js/app.js
@@ -261,6 +261,30 @@ class Timer extends React.Component {
   }
 }
 
+class TimerActionButton extends React.Component {
+  render() {
+    if (this.props.timerIsRunning) {
+      return (
+        <div
+          className='ui bottom attached red basic button'
+          onClick={this.props.onStopClick}
+        >
+          Stop
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className='ui bottom attached green basic button'
+          onClick={this.props.onStartClick}
+        >
+          Start
+        </div>
+      );
+    }
+  }
+}
+
 class TimerForm extends React.Component {
   state = {
     title: this.props.title || '',
